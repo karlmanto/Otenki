5dec548ba2fcdf9f39cfd90d76c719151b96174c
Karl Manto <karlmanto0@gmail.com>
2023-04-05 16:41:18 -0700

    Create Learn page

--- client/src/pages/Learn/Learn.js
+++ client/src/pages/Learn/Learn.js
@@ -1,7 +1,21 @@
 import './Learn.scss';
+import mountFuji from '../../assets/videos/mount-fuji.mp4';
+import road from '../../assets/videos/road.mp4';
 
 export default function Learn() {
     return (
-        <h1>Learn</h1>
+        <main className='learn'>
+            <h1 className='learn__header'>What would you like to learn?</h1>
+            <section className='learn__cards'>
+                <div className='learn__card'>
+                    <div className='learn__overlay'>Hiragana ひらがな</div>
+                    <video className='learn__video' src={mountFuji} autoPlay loop muted />
+                </div>
+                <div className='learn__card'>
+                    <div className='learn__overlay'>Katakana カタカナ</div>
+                    <video className='learn__video' src={road} autoPlay loop muted />
+                </div>
+            </section>
+        </main>
     )
 }
